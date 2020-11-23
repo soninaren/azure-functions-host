0dbb15f2e5a239f88006924f49c4f1b1d8505775
Bala <balag@outlook.com>
2020-11-23 14:04:13 -0800

    2

--- src/WebJobs.Script.WebHost/Controllers/InstanceController.cs
+++ src/WebJobs.Script.WebHost/Controllers/InstanceController.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
